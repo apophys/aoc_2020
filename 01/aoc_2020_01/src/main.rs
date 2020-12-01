deacacbfc268cd67f249fc76c6afc7f726fe04c7
Milan Kubík <email@kubikmilan.sk>
2020-12-02 00:58:02 +0100

    Implement part 2 of the task, search for three numbers

--- 01/aoc_2020_01/src/main.rs
+++ 01/aoc_2020_01/src/main.rs
@@ -1,5 +1,3 @@
-// devel time yay
-#![allow(unused_imports,dead_code,unused_variables,unused_mut)]
 
 use std::error::Error;
 
@@ -24,14 +22,14 @@ fn load_numbers(filename: &str) -> AOCResult<Vec<i32>> {
 	Ok(numbers)
 }
 
-fn compute_nice_pair(numbers_list: Vec<i32>, expected_sum: i32) -> Option<(i32, i32)> {
+fn compute_nice_pair(numbers_list: &Vec<i32>, expected_sum: i32) -> Option<(i32, i32)> {
 	let mut counter: usize = 0;
 
 	while counter < numbers_list.len() {
 		let current = numbers_list[counter];
 		for i in &numbers_list[counter..] {
 			if current + i == expected_sum {
-				eprintln!("Found pair summing up to {}: {} + {}", expected_sum, i, current);
+				eprintln!("Found pair adding up to {}: {} + {}", expected_sum, i, current);
 				return Some((current, *i))
 			}
 		}
@@ -41,12 +39,39 @@ fn compute_nice_pair(numbers_list: Vec<i32>, expected_sum: i32) -> Option<(i32, 
 
 }
 
+fn compute_nice_triplet(numbers_list: &Vec<i32>, expected_sum: i32) -> Option<(i32, i32, i32)> {
+	let mut counter: usize = 0;
+	while counter < numbers_list.len() {
+		let current = numbers_list[counter];
+
+		let mut inner_counter: usize = 0;
+		let outer_slice = &numbers_list[counter..];
+		while inner_counter < outer_slice.len() {
+			let inner_value = outer_slice[inner_counter];
+			for i in &outer_slice[inner_counter..] {
+				if current + inner_value + i == expected_sum {
+					eprintln!("Found three values adding up to {}: {} + {} + {}", expected_sum, i, inner_value, current);
+					return Some((current, inner_value, *i))
+				}
+
+			}
+			inner_counter += 1;
+		}
+		counter += 1;
+	}
+	None
+}
+
 fn main() -> AOCResult<()>{
 	if let Some(filename) = env::args().skip(1).next() {
 		let numbers = load_numbers(&filename)?;
 
-		if let Some(pair) = compute_nice_pair(numbers, 2020) {
+		if let Some(pair) = compute_nice_pair(&numbers, 2020) {
 			eprintln!("Result for AoC task 1: {} ", pair.0 * pair.1);
+		}
+
+		if let Some(triplet) = compute_nice_triplet(&numbers, 2020) {
+			eprintln!("Result for AoC task 1 part 2: {} ", triplet.0 * triplet.1 * triplet.2);
 		}
 
 		Ok(())
