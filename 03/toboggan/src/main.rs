f450d5692f46a5a2b8c3397c1e3299c8ccb98a6b
Milan Kubík <email@kubikmilan.sk>
2021-03-01 10:48:20 +0100

    More simplification

--- 03/toboggan/src/main.rs
+++ 03/toboggan/src/main.rs
@@ -1,8 +1,7 @@
 mod map;
 
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
 
 use crate::map::Map;
 
@@ -29,10 +28,7 @@ fn count_toboggan_trees(map: &Map, rules: SlopeTraversal) -> usize {
 
 fn main() -> std::io::Result<()> {
     if let Some(filename) = env::args().nth(1) {
-        let mut file = File::open(&filename)?;
-        let mut file_content = String::new();
-
-        file.read_to_string(&mut file_content)?;
+        let file_content = fs::read_to_string(&filename)?;
 
         let rule_list = [
             SlopeTraversal { right: 1, down: 1 },
