78158ed83be188d0c86b3baae032e21c9b6457f4
Milan Kubík <email@kubikmilan.sk>
2021-03-20 20:23:15 +0100

    Use the iterator count method instead of collecting a vector

--- 04/passport-processing/src/main.rs
+++ 04/passport-processing/src/main.rs
@@ -5,25 +5,25 @@ use std::fs;
 
 use passport::Passport;
 
-
 fn main() -> std::io::Result<()> {
     if let Some(filename) = env::args().nth(1) {
         let content = fs::read_to_string(&filename)?;
 
-        let passports: Result<Vec<_>, _> = content.split("\n\n").map(Passport::new_from_slice).collect();
+        let passports: Result<Vec<_>, _> = content
+            .split("\n\n")
+            .map(Passport::new_from_slice)
+            .collect();
 
         if let Ok(passports) = passports {
-        	let result1: Vec<_> = passports.iter().filter(|x| x.is_valid()).collect();
-        	println!("There is {} valid passwords", result1.len());
+            let result1 = passports.iter().filter(|x| x.is_valid()).count();
+            println!("There is {} valid passwords", result1);
 
-        	let result2: Vec<_> = passports.iter().filter(|x| x.is_strictly_valid()).collect();
-        	println!("There is {} strictly valid passwords", result2.len());
+            let result2 = passports.iter().filter(|x| x.is_strictly_valid()).count();
+            println!("There is {} strictly valid passwords", result2);
+        } else {
+            eprintln!("Couldn't parse the input data.");
+            std::process::exit(1);
         }
-        else {
-        	eprintln!("Couldn't parse the input data.");
-        	std::process::exit(1);
-        }
-
     }
     Ok(())
 }
